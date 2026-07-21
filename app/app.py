fd7e770d45ab16901fbeedfd49f7019e3d388d1b
Jesus Reyes <Jesusfranciscoreyesg2006@gmail.com>
2026-07-21 00:20:44 -0400

    Railway

--- app/app.py
+++ app/app.py
@@ -92,4 +92,5 @@ def capitulo():
     return render_template('capitulo.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
